4bb83d1b8ee5b78612d4eb255b552f78c2527827
Camila Millán <cfmillans@gmail.com>
2026-07-17 23:08:24 -0400

    V2 Batch 4: Service Worker and PWA Manifest

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'johan-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/img/hero.webp'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) return response;
+        return fetch(event.request);
+      })
+  );
+});
